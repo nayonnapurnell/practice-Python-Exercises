f919f727a8b3dd403331ffa08bc85404479c947e
“Nayonna <Nayonna Purnell>
2020-02-25 23:59:49 -0500

    Updated Exercise 1

--- practice-python-exercise-1.py
+++ practice-python-exercise-1.py
@@ -11,14 +11,21 @@ Print out that many copies of the previous message on separate lines. (Hint: the
 #Ask the user to enter their name.
 name = input("What's your name? ")
 
-#Ask the user to enter their age.
+#Ask the user to enter their age
 age = input("How old are you? ")
 
+#Convert age into an integer
+age = int(age)
+
 #Determine how many years the user has until they are 100.
-year = 100 - age
+yearsUntil100 = int(100 - age)
+
+#Convert yearsUntil100 into an integer
+yearsUntil100 = int(yearsUntil100)
 
 #subtract 100 years from the users age.
 
 #report the 
-print("Did you know that in the year " + year + "you will be 100 years old?")
+print(name + ", Did you know that you have " yearsUntil100 "until you will be 100?")
+#print("Did you know that in the year " + year + "you will be 100 years old?")
 
